14f9a2a6559499e15d1dc716175b1a54903b6419
bayyaramharsha <bayyaramharsha@gmail.com>
2023-05-16 13:57:08 +0530

    only js final working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// $("#openmodal").click(function() {
-//     $("#container").css("display", "block");
-//     $("#popup").css("display", "block");
-// });
-
-// $(".cancel").click(function() {
-//     $("#container").fadeOut();
-//     $("#popup").fadeOut();
-// });
-
-
 function openmodel() {
     document.getElementById("container").style.display = "block";
     document.getElementById("popup").style.display = "block";
