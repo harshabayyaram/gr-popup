5b7052560b0171831e752a5db0dceb5f8e9ce95f
bayyaramharsha <bayyaramharsha@gmail.com>
2023-05-16 11:45:26 +0530

    working cp codepen

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("#open").click(function() {
+    $("#a").css("display", "block");
+    $("#b").css("display", "block");
+});
+
+
+$(".cancel").click(function() {
+    $("#a").fadeOut();
+    $("#b").fadeOut();
+});
